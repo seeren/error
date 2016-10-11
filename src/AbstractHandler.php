5a9da2eea095e4a658a31a9d6ea05e7cc89713d7
Cyril <consultant@seeren.fr>
2016-10-11 03:54:53 +0200

    Create AbstractHandler

---
+++ src/AbstractHandler.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * This file contain Seeren\Error\AbstractHandler class
+ *     __
+ *    / /__ __ __ __ __ __
+ *   / // // // // // // /
+ *  /_// // // // // // /
+ *    /_//_//_//_//_//_/
+ *
+ * @copyright (c) Cyril Ichti <consultant@seeren.fr>
+ * @link http://www.seeren.fr/ Seeren
+ * @version 1.0.1
+ */
+
+namespace Seeren\Error;
+
+/**
+ * Class for handle error
+ * 
+ * @category Seeren
+ * @package Error
+ * @abstract
+ */
+abstract class AbstractHandler
+{
+
+   /**
+    * Construct Handler
+    * 
+    * @return null
+    */
+   protected function __construct()
+   {
+   }
+
+   /**
+    * Shutdown method
+    *
+    * @return null
+    */
+   public final function shutdown()
+   {
+       if (($error = error_get_last())) {
+           $this->handle(
+               $error[static::TYPE],
+               $error[static::MESSAGE],
+               $error[static::FILE],
+               $error[static::LINE]
+           );
+       }
+   }
+
+   /**
+    * Register
+    *
+    * @return HandlerInterface static
+    */
+   public final function register(): HandlerInterface
+   {
+       set_error_handler([$this, "handle"]);
+       register_shutdown_function([$this, "shutdown"]);
+       return $this;
+   }
+
+   /**
+    * Unregister
+    *
+    * @return HandlerInterface static
+    */
+   public final function unregister(): HandlerInterface
+   {
+       restore_error_handler();
+       register_shutdown_function(function () {});
+       return $this;
+   }
+}
